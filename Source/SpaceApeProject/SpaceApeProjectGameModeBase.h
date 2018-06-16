ca65558ce290eae71b1e0d1921e7e222bbeeafef
KrisPearson <pearsonk1@hotmail.co.uk>
2018-06-16 15:24:41 +0100

    Initial commit

---
+++ Source/SpaceApeProject/SpaceApeProjectGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SpaceApeProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACEAPEPROJECT_API ASpaceApeProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
